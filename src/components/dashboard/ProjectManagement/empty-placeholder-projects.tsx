aebf0c0360eb09859087c3085bf60a226ecb3fc1
Tomas Cordero <tomas.cordero.esp@gmail.com>
2024-03-06 12:49:43 -0500

    feat: icons gradient

--- src/components/dashboard/ProjectManagement/empty-placeholder-projects.tsx
+++ src/components/dashboard/ProjectManagement/empty-placeholder-projects.tsx
@@ -1,24 +1,27 @@
-
 "use client";
 
 import { useEffect, useRef } from "react";
 import { Player } from "@lordicon/react";
 import SubmitProjectDialog from "@/components/dashboard/ProjectManagement/submit-project-dialog";
-import wiredEnvironemnt from "@/lib/foresta-icons/wired-outline-945-dividends.json";
+import wiredEnvironemntLight from "@/lib/foresta-icons/wired-outline-945-dividends.json";
+import wiredEnvironemntDark from "@/lib/foresta-dark/wired-gradient-437-environment-eco-care.json";
+import { LordIcon } from "@/lib/lordicon/lord-icon";
+
+import { useTheme } from "next-themes";
 
 export function ProjectEmptyPlaceholder() {
-  const playerRef = useRef<Player>(null);
-  useEffect(() => {
-    playerRef.current?.playFromBeginning();
-  }, []);
+  const { theme } = useTheme();
+  const wiredEnvironemnt = theme === "dark" ? wiredEnvironemntDark : wiredEnvironemntLight;
+
+    const ecoIconDataUri = `data:application/json;base64,${Buffer.from(JSON.stringify(wiredEnvironemnt)).toString("base64")}`;
+
   return (
     <div className="flex h-[450px] shrink-0 items-center justify-center rounded-md border border-dashed">
       <div className="mx-auto flex max-w-[420px] flex-col items-center justify-center text-center">
-      <Player
-          ref={playerRef}
-          icon={wiredEnvironemnt}
-          state="loop-cycle"
-          onComplete={() => playerRef.current?.playFromBeginning()}
+        <LordIcon
+          src={ecoIconDataUri}
+          trigger="loop-on-hover"
+          colors={{ primary: "#303f9f" }}
           size={100}
         />
         <h3 className="mt-4 text-lg font-semibold">Submit a New Project</h3>
@@ -30,3 +33,6 @@ export function ProjectEmptyPlaceholder() {
     </div>
   );
 }
+
+// primary: 66ee78
+// secondary: c69cf4
--- src/components/dashboard/ProjectManagement/empty-placeholder-tokens.tsx
+++ src/components/dashboard/ProjectManagement/empty-placeholder-tokens.tsx
@@ -12,22 +12,30 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import wiredInvestmentAnimation from "@/lib/foresta-icons/wired-outline-947-investment.json";
+import wiredInvestmentAnimationLight from "@/lib/foresta-icons/wired-outline-947-investment.json";
+import wiredInvestmentAnimationDark from "@/lib/foresta-dark/wired-gradient-947-investment.json";
+import { LordIcon } from "@/lib/lordicon/lord-icon";
+import { useTheme } from "next-themes";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
 
 export function TokensEmptyPlaceholder() {
-  const playerRef = useRef<Player>(null);
-  useEffect(() => {
-    playerRef.current?.playFromBeginning();
-  }, []);
+  const { theme } = useTheme();
+  const wiredEnvironemnt = theme === "dark" ? wiredInvestmentAnimationDark : wiredInvestmentAnimationLight;
+
+    const ecoIconDataUri = `data:application/json;base64,${Buffer.from(JSON.stringify(wiredEnvironemnt)).toString("base64")}`;
+
 
   return (
     <div className="flex h-[450px] shrink-0 items-center justify-center rounded-md border border-dashed">
       <div className="mx-auto flex max-w-[420px] flex-col items-center justify-center text-center">
-        <Player ref={playerRef} icon={wiredInvestmentAnimation} onComplete={() => playerRef.current?.playFromBeginning()} size={100}/>
-        <h3 className="mt-4 text-lg font-semibold">Mint New Tokens</h3>
+      <LordIcon
+          src={ecoIconDataUri}
+          trigger="loop-on-hover"
+          colors={{ primary: "#303f9f" }}
+          size={100}
+        />        <h3 className="mt-4 text-lg font-semibold">Mint New Tokens</h3>
         <p className="mb-4 mt-2 text-sm text-muted-foreground">
           Ready to convert your project contributions into tokens? Start here.
         </p>
